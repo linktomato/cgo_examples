0af74161d76a1dc6186038f7c543eec87f4cb3dd
linkaitao <linkaitao@qiku.com>
2020-08-06 18:02:49 +0800

    update struct_type

--- type_conversion/struct_type.go
+++ type_conversion/struct_type.go
@@ -4,10 +4,21 @@ package type_conversion
 typedef struct {
 	char *name;
 	int age;
+	// 用Go关键字命名的变量名，在Go中以该变量名前加下划线访问，此处为"_type"。
+	// 如果存在成员变量名为"_type"，则此变量会被Go忽略。
+	// Member's name conflicts with Go's keyword,
+	// Go will access it by using name "_type".
+	// If there is some one named "_type" then this
+    // member will be ignored in Go.
+	char *type;
+	char *_type;
 } programer;
 
 programer me = {
-	"tomato",30
+	"tomato",
+	30,
+	"ignored",
+	"primary engineer"
 };
 */
 import "C"
@@ -19,11 +30,13 @@ func GetSetStructTypeVariables() {
 	fmt.Println(C.GoString(C.me.name))
 	// print: 30
 	fmt.Println(C.me.age)
+	// 如果成员变量的名字和
+	// print: primary engineer
+	fmt.Println(C.GoString(C.me._type))
 
-	
 	// ------ set value ------
-	// 修改结构体字段值
-	// set field value
+	// 修改结构体成员变量值
+	// set struct member value
 	C.me.name = C.CString("old tomato")
 	C.me.age = C.int(60)
 	// print: old tomato
@@ -34,8 +47,8 @@ func GetSetStructTypeVariables() {
 	// 创建新的结构体变量
 	// set a new struct variable
 	var me = C.programer{
-		name: C.CString("super tomato"),
-		age:  C.int(120),
+		name:  C.CString("super tomato"),
+		age:   C.int(120),
 	}
 	C.me = me
 	// print: super tomato
