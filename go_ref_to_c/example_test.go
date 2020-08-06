20b03404a4f0d764ad73380438033f9a674d28f1
linkt2018 <1650476351@qq.com>
2020-08-06 14:46:47 +0800

    update doc and rm some files

--- go_ref_to_c/example_test.go
+++
@@ -1,8 +0,0 @@
-package go_ref_to_c
-
-import "testing"
-import "github.com/stretchr/testify/assert"
-
-func TestCAdd(t *testing.T) {
-	assert.Equal(t, 3, CAdd(1, 2))
-}
--- go_ref_to_c/mul_rtn_vals.go
+++
@@ -1,1 +0,0 @@
-package go_ref_to_c
