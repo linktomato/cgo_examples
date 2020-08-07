594c6324a3dfd35b12a7e6c0e74fb559e856bcc9
linkaitao <linkaitao@qiku.com>
2020-08-07 11:36:50 +0800

    update enum_type

--- type_conversion/enum_type.go
+++ type_conversion/enum_type.go
@@ -1,1 +1,24 @@
 package type_conversion
+
+// see https://chai2010.cn/advanced-go-programming-book/ch2-cgo/ch2-03-cgo-types.html
+
+/*
+enum e {
+	ONE,
+	TWO
+};
+enum e e_val = ONE;
+*/
+import "C"
+import "fmt"
+
+func GetSetEnumTypeVariable()  {
+	// ------ get value ------
+	// print: 0(ONE)
+	fmt.Println(C.e_val)
+
+	// ------ set value ------
+	C.e_val = C.TWO
+	// print: 1(TWO)
+	fmt.Println(C.e_val)
+}
---
+++ type_conversion/enum_type_test.go
@@ -0,0 +1,7 @@
+package type_conversion
+
+import "testing"
+
+func TestGetSetEnumTypeVariable(t *testing.T) {
+	GetSetEnumTypeVariable()
+}
